9653fd67f5bf5712756023261912c544b639ff73
Manos Kounelakis <pkounelios@gmail.com>
2018-10-01 21:40:55 +0300

    Edit hero info

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -3,7 +3,7 @@ import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { HeroService }  from '../hero.service';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -11,12 +11,12 @@ import { HeroService }  from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero:Hero;
+  @Input() hero: Hero;
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) {}
+  ) { }
 
   ngOnInit() {
     this.getHero();
@@ -27,7 +27,12 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
-  goBack():void{
+  goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
