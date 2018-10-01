edb8fa2ee272e7392a81726c86d78b73d40d0a7b
Manos Kounelakis <pkounelios@gmail.com>
2018-10-01 21:26:34 +0300

    in memory data initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,9 @@ import { HeroService } from './hero.service';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,7 +22,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService,{dataEncapsulation:false})
 
   ],
   providers: [HeroService],
